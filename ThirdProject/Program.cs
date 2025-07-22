1a5e992560611685b48f7832b72bf5526a2a1c0f
Osama Elazab <osamaelazab809@gmail.com>
2025-07-22 03:12:06 +0300

    c

--- ThirdProject/Program.cs
+++ ThirdProject/Program.cs
@@ -5,10 +5,10 @@
         static void Main(string[] args)
         {
             Duration D1 = new Duration(1, 10, 15);
-            Console.WriteLine(D1);
+            Console.WriteLine("D1:" + D1);
 
             D1 = new Duration(3600);
-            Console.WriteLine(D1);
+            Console.WriteLine("D1:" + D1);
 
             Duration O = new Duration(3600);
             Console.WriteLine("Equals(): " + O.Equals(D1));
@@ -16,10 +16,10 @@
             Console.WriteLine("O.GetHashCode() :" + O.GetHashCode());
 
             Duration D2 = new Duration(7800);
-            Console.WriteLine(D2);
+            Console.WriteLine("D2:" + D2);
 
             Duration D3 = new Duration(666);
-            Console.WriteLine(D3);
+            Console.WriteLine("D3:" + D3);
 
             D3 = D1 + D2;
             Console.WriteLine("D3 = D1 + D2 => " + D3);
@@ -40,8 +40,7 @@
             Console.WriteLine("D1 - D2 => " + D1);
 
             Console.WriteLine(D1 > D2 ? "D1 > D2" : "D1 <= D2");
-
-            DateTime dt = (DateTime)D1;
+            DateTime dt = (DateTime)D3;
             Console.WriteLine("Cast to DateTime => " + dt);
         }
     }
